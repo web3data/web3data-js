58d4c9a95830d292f1f394a4948dad3407b64013
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-07 16:28:36 -0700

    Finished implementing all methods and modifiers

--- test/web3data.test.js
+++ test/web3data.test.js
@@ -51,9 +51,26 @@ test('web3data should have the correct blockchain id', t => {
 
 test.todo('web3data should have the correct blockchain slug') // TBD
 
+test('throws exception when no config object is supplied', t => {
+  const error = t.throws(() => { new Web3data() }, Error);
+  t.is(error.message, "No configuration object supplied");
+})
+
+test('throws exception when no api key is supplied', t => {
+  const error = t.throws(() => { new Web3data({}) }, Error);
+  t.is(error.message, "No api key supplied");
+})
+
+test('throws exception when no blockchainid is supplied', t => {
+  const error = t.throws(() => { new Web3data({'apiKey':'apikey'}) }, Error);
+  t.is(error.message, "No Blockchain specified");
+})
 
 /* Test addresses method */
-test.todo('throws exception when calling 'addresses' without hash')
+test('throws exception when calling \'addresses\' without hash', t => {
+  const error = t.throws(() => { web3data.addresses() }, Error);
+  t.is(error.message, 'No address hash provided');
+})
 
 // Test info method
 test('Successfully gets address information', async t => {
@@ -67,22 +84,48 @@ test('Successfully gets address information', async t => {
   let addressInfo = await web3data.addresses('0x314159265dd8dbb310642f98f50c066173c1259b').info().retrieve()
   t.is(addressInfo.status, 200)
 
-
   nockDone();
   nock.back.setMode('wild');
 })
 
 // Test stats method
-test.todo('gets address stats')
+test('gets address stats', async t => {
+  nock.back.setMode('record');
+
+  const { nockDone } = await nock.back(
+    'address-stats.json',
+    defaultOptions,
+  );
+
+  let addressStats= await web3data.addresses('0x314159265dd8dbb310642f98f50c066173c1259b').stats().retrieve()
+  console.log(addressStats.status)
+  t.is(addressStats.status, 200)
+
+  nockDone();
+  nock.back.setMode('wild');
+})
 
 // Test logs method
-test.todo('gets address logs')
+test('gets address logs', async t => {
+  nock.back.setMode('record');
+
+  const { nockDone } = await nock.back(
+    'address-logs.json',
+    defaultOptions,
+  );
+
+  let addressLogs = await web3data.addresses('0x314159265dd8dbb310642f98f50c066173c1259b').logs().retrieve()
+  t.is(addressLogs.status, 200)
+
+  nockDone();
+  nock.back.setMode('wild');
+})
 
 // Test transactions method
 test.todo('gets all transactions of the address')
 test.todo('gets single transaction of the address')
 test.todo('gets non existent transaction of the address and receives error')
-
+//, t => {}
 // Test messages method
 test.todo('gets all messages of the address')
 test.todo('gets single messages of the address')
@@ -94,3 +137,21 @@ test.todo('gets single token of the address')
 /*
   Testing Modifiers
 */
+
+test('Filters properly', async t => {
+
+  const filterOpts = {'blockNumber':6237323}
+
+  nock.back.setMode('record');
+
+  const { nockDone } = await nock.back(
+    'address-logs-filter.json',
+    defaultOptions,
+  );
+
+  let addressLogs = await web3data.addresses('0x314159265dd8dbb310642f98f50c066173c1259b').logs().filte(filterOpts).retrieve()
+  t.is(addressLogs.status, 200)
+
+  nockDone();
+  nock.back.setMode('wild');
+})
