49c60f3d92044f50eb7e2025a09bded136d62ae1
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-12 16:46:23 -0700

    Build revisions:  - Move back to parcel  - Create two build options 'node' 'browser'  - Update gitignore

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import browserConfig from './rollup.browser.config.js';
-import nodeConfig from './rollup.node.config.js';
-
-export default commandLineArgs => {
-  if (commandLineArgs.browserConfig === true) {
-    return browserConfig;
-  }
-  return nodeConfig;
-}
