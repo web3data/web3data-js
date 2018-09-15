741676cf1eed1f4ca2087c142218c0460809d754
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-14 23:16:06 -0700

    Slight refactor:   - Moved retrieve function from web3data.js to it's own file   - All tests on web3data (node) run flawlessly   - [W.I.P.] Browser tests fail bc babel-polly fill is required     working out way to include it without it being bundled

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import 'babel-polyfill'
-module.exports = require('./lib/web3data')
+import Web3Data from './lib/web3data'
+import retrieve from './lib/node/retrieve'
+
+// Overriden to use the for browsers
+Web3Data.prototype.retrieve = retrieve
+module.exports = Web3Data
