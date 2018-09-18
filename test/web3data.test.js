537c171275232f8f744a93f4e3cdd27f95b66b21
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-18 09:27:02 -0700

    Updates and travis fix:  - Updated ky to 0.3.0  - removed API_KEY from test files  - update travis build badge in README

--- test/web3data.test.js
+++ test/web3data.test.js
@@ -4,9 +4,15 @@ import test from 'ava'
 import dotenv from 'dotenv'
 dotenv.load()
 
-if(!process.env.API_KEY) {
-  console.error("Must set API_KEY value in .env file") //TODO: Elaborate on how to get it
-  process.exit(1)
+let API_KEY
+if(process.env.API_KEY) {
+  API_KEY = process.env.API_KEY
+} else {
+  console.warn("Must set API_KEY value in .env file, \n\
+  Create an account on amberdata.io to obtain one")
+
+  // Dumby key; won't work on live net
+  API_KEY = 'lbK5e0cae7xf494P3c8Q1od19h41b3fa973'
 }
 
 /**********************************
@@ -16,7 +22,7 @@ const ADDRESS = '0x873029fead9cc9c05860557576ca1577f420a801'
 const TX_HASH = '0x7a3dbdc6f5b8b748d972ee9e35ecea6ff62a624816c944bf2419c430156c54ba'
 const TOKEN_HASH = '0x744d70fdbe2ba4cf95131626614a1763df805b9e' /* Status Network Token */
 const SLUG = ''
-const API_KEY = process.env.API_KEY
+
 const BLOCKCHAIN_ID = '1c9c969065fcd1cf'/* Ethereum-mainnet */
 
 const CONFIG = {
