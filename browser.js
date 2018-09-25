02b5c50380e1d21055d2f09a5c8b857c5a0a30ac
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-24 17:26:57 -0700

    Fixed linting error and remvoed .cache

--- browser.js
+++ browser.js
@@ -4,9 +4,9 @@ import retrieve from './lib/browser/retrieve'
 // Overriden to use the for browsers
 Web3Data.prototype.retrieve = retrieve
 
-// eslint-disable-next-line
+// eslint-disable-next-line no-undef
 if (typeof window !== 'undefined' && typeof window.Web3Data === 'undefined') {
-  window.Web3Data = Web3Data // eslint-disable-line
+  window.Web3Data = Web3Data // eslint-disable-line no-undef
 }
 
 module.exports = Web3Data
