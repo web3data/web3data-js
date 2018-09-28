32547a2fb49d40d428031af9c3dcfc4b003dadc8
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-28 10:31:20 -0700

    fixed const & import issues

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require('babel-polyfill')
+const Web3Data = require('./lib/web3data')
+const retrieve = require('./lib/node/retrieve')
 
-let Web3Data = require('./lib/web3data')
-let retrieve = require('./lib/node/retrieve')
 // Overriden to use the for browsers
 Web3Data.prototype.retrieve = retrieve
 module.exports = Web3Data
