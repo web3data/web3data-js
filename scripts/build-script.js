999affecd1ea9297bae0cf43311927b225fb33ff
Taylor Dawson <taylorjdawson@gmail.com>
2019-07-11 15:24:56 -0700

    updated build scripts

--- scripts/build-script.js
+++ scripts/build-script.js
@@ -1,45 +1,10 @@
 require('dotenv').load()
-let helpers = require('./helpers');
+const helpers = require('./helpers');
 const Bundler = require('parcel-bundler')
 const Path = require('path')
-var babel = require("babel-core")
-let fs = require('fs')
+const fs = require('fs')
 
-async function runBundle() {
-
-  if(process.argv[2] === 'clean') {
-    helpers.deleteDir('./dist')
-    console.log('Removed dist')
-
-    helpers.deleteDir('./cache')
-    console.log('Removed cache')
-  }
-
-  switch(process.env.TARGET) {
-    case 'ALL':
-      await bundelNode()
-    case 'BROWSER':
-      await bundelBrowser()
-    break;
-    case 'NODE':
-      await bundelNode()
-    break;
-  }
-}
-
-async function transpileKy() {
-   babel.transformFile('node_modules/ky/index.js', (err, res) => {
-     if(err) {
-       console.error(err)
-     } else {
-       fs.writeFile('./kympiled.js', res.code, (err) => {
-         if (err) throw err;
-       })
-     }
-   })
-}
-
-async function bundelBrowser() {
+bundleBrowser = async () => {
 
   // Bundler options for browser
   const options = {
@@ -49,32 +14,19 @@ async function bundelBrowser() {
     detailedReport: true
   };
 
-  await transpileKy()
-
-  const entryFile = Path.join(__dirname, '../browser.js');
-
+  const entryFile = Path.join(__dirname, '../src/web3data.js');
+  console.log(entryFile)
   const bundler = new Bundler(entryFile, options);
-  const Bundle = await bundler.bundle();
-  fs.unlink('./kympiled.js', (err) => {
-  if (err) throw err;
-  })
+  return bundler.bundle();
 }
 
-async function bundelNode() {
+(async () => {
+  if(process.argv[2] === 'clean') {
+    helpers.deleteDir('./dist')
+    console.log('Removed dist')
 
-  // Bundler options for node
-  const options = {
-    outDir: './dist/node',
-    outFile: 'web3data.js',
-    target: 'node',
-    watch: false,
-    minify: false,
-    detailedReport: true
-  };
-
-  const entryFile = Path.join(__dirname, '../index.js');
-  const bundler = new Bundler(entryFile, options);
-  const Bundle = await bundler.bundle();
-}
-
-runBundle();
+    helpers.deleteDir('./cache')
+    console.log('Removed cache')
+  }
+  await bundleBrowser()
+})()
