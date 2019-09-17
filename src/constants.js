32977cb6a19f8f1571363fe67cdd241206b0bcdd
Trevor Clarke <trevorjtclarke@gmail.com>
2019-09-16 17:05:07 -0700

    added market data methods

--- src/constants.js
+++ src/constants.js
@@ -22,6 +22,7 @@ module.exports.ERROR_MESSAGE_ADDRESS_NO_ADDRESS = 'No address supplied'
 module.exports.ERROR_MESSAGE_CONTRACT_NO_ADDRESS =
   'No contract address supplied'
 module.exports.ERROR_MESSAGE_BLOCK_NO_ID = 'No block number or hash supplied'
+module.exports.ERROR_MESSAGE_MARKET_NO_PAIR = 'No market pair supplied'
 module.exports.ERROR_MESSAGE_SIGNATURE_NO_HASH = 'No signature hash supplied'
 
 /* Ethereum based methods */
