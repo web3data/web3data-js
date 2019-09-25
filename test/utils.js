75eff1027bfab8192f878509bccc3038f4461937
Taylor Dawson <taylorjdawson@gmail.com>
2019-09-24 17:01:27 -0700

    remove key string - fix recordings not matching recs now remove api key from url and query

--- test/utils.js
+++ test/utils.js
@@ -8,7 +8,7 @@ export const setUpPolly = (recordingName) => {
     Polly.register(FSPersister);
     Polly.register(NodeHttpAdapter);
 
-    const polly = new Polly(`${recordingName}_${Date.now()}` , {
+    const polly = new Polly(`${recordingName}` , { // _${Date.now()}
         adapters: ['node-http'],
         persister: 'fs',
         persisterOptions: {
@@ -19,12 +19,24 @@ export const setUpPolly = (recordingName) => {
         matchRequestsBy: {
             headers: {
                 exclude: ['x-api-key']
+            },
+            url: {
+                query(query) {
+                    delete query['x-api-key']
+                    return query
+                }
             }
         }
     });
     const { server } = polly;
     server.any().on('beforePersist', (req, recording) => {
         recording.request.headers = recording.request.headers.filter(({ name }) => name !== 'x-api-key')
+        recording.request.queryString = recording.request.queryString.filter(({ name }) => name !== 'x-api-key')
+
+        /* Remove the api key from the query params*/
+        const url = new URL(recording.request.url)
+        url.searchParams.set('x-api-key', '')
+        recording.request.url = url
     });
     return polly
 }
