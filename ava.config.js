986fb77876960fff658a527c27c67c4b747b8282
Taylor Dawson <taylorjdawson@gmail.com>
2020-02-04 14:10:35 -0800

    fix(websocket-disconnect-error): Update recs + linting

--- ava.config.js
+++ ava.config.js
@@ -4,14 +4,6 @@ export default {
   cache: true,
   concurrency: 5,
   require: ['@babel/register'],
-<<<<<<< HEAD
   babel: true,
-=======
-  babel: {
-    testOptions: {
-      presets: [['@ava/stage-4', false]]
-    }
-  },
->>>>>>> fix/update-deps
   inherit: true
 }
